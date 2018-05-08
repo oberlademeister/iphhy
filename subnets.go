7f245ee37b56379722da7142bbbdd8fb0c718223
Holger Fuessler <holger@fuessler.eu>
2018-05-08 23:44:31 +0200

    adding subnet magic

--- subnets.go
+++ subnets.go
@@ -11,3 +11,26 @@ func (i I4) Overlaps(i2 I4) bool {
 	}
 	return true
 }
+
+// NumIPs gives the number of IPs in the subnet
+func (i I4) NumIPs() int {
+	return 1 << uint32(32-i.maskBits)
+}
+
+// NumHosts gives the number of hosts
+func (i I4) NumHosts() int {
+	switch i.maskBits {
+	case 32:
+		return 0
+	case 31:
+		return 2
+	case 30:
+		return 2
+	}
+	return i.NumIPs() - 2
+}
+
+// SubnetOffset gives the +-i subnet with the same mask, i.e. the IP address that is shifted o times the number of hosts upwards or downwards
+func (i I4) SubnetOffset(o int) I4 {
+	return I4{ip: i.ip + uint32(o*i.NumIPs()), maskBits: i.maskBits}
+}
--- subnets_test.go
+++ subnets_test.go
@@ -1,6 +1,9 @@
 package iphhy
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestI4_Overlaps(t *testing.T) {
 	tests := []struct {
@@ -44,6 +47,66 @@ func TestI4_Overlaps(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.i1.Overlaps(tt.i2); got != tt.want {
 				t.Errorf("I4.Overlaps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestI4_NumIPs(t *testing.T) {
+	tests := []struct {
+		name string
+		i    I4
+		want int
+	}{
+		{"sn32", MustNewI4("192.168.0.1/32"), 1},
+		{"sn31", MustNewI4("192.168.0.1/31"), 2},
+		{"sn30", MustNewI4("192.168.0.1/30"), 4},
+		{"sn24", MustNewI4("192.168.0.1/24"), 256},
+		{"sn16", MustNewI4("192.168.0.1/16"), 65536},
+		{"sn0", MustNewI4("192.168.0.1/0"), 4294967296},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.i.NumIPs(); got != tt.want {
+				t.Errorf("I4.NumIPs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestI4_NumHosts(t *testing.T) {
+	tests := []struct {
+		name string
+		i    I4
+		want int
+	}{
+		{"sn32", MustNewI4("192.168.0.1/32"), 1},
+		{"sn31", MustNewI4("192.168.0.1/31"), 2},
+		{"sn30", MustNewI4("192.168.0.1/30"), 2},
+		{"sn24", MustNewI4("192.168.0.1/24"), 254},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.i.NumHosts(); got != tt.want {
+				t.Errorf("I4.NumHosts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestI4_SubnetOffset(t *testing.T) {
+	tests := []struct {
+		name string
+		i    I4
+		o    int
+		want I4
+	}{
+		{"t1", MustNewI4("192.168.0.1/24"), -1, MustNewI4("192.167.255.1/24")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.i.SubnetOffset(tt.o); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("I4.SubnetOffset() = %v, want %v", got, tt.want)
 			}
 		})
 	}
