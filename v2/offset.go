231135a28d04f9a75fb973fa2a387d2d7ffd0f42
Torsten Fellhauer <Torsten.Fellhauer@sap.com>
2024-11-04 10:58:41 +0100

    adding subnetoffset

--- v2/offset.go
+++ v2/offset.go
@@ -1,6 +1,9 @@
 package iphhy
 
-import "math/big"
+import (
+	"fmt"
+	"math/big"
+)
 
 // Offset returns a fresh IP struct set off by int
 func (ip *IP) Offset(offset int64) *IP {
@@ -9,4 +12,28 @@ func (ip *IP) Offset(offset int64) *IP {
 	ret := NewFromBigInt(i)
 	ret.SetMaskInplace(ip.mask)
 	return ret
+}
+
+// SubnetOffset returns a fresh IP struct set off by int
+func (ip *IP) SubnetOffset(offset int64) (*IP, error) {
+	lower := ip.Network().BigInt()
+	upper := ip.Broadcast().BigInt()
+	var newIPInt = big.NewInt(0)
+	switch {
+	case offset == 0:
+		newIPInt = lower
+	case offset < 0:
+		o := big.NewInt(-1*offset - 1)
+		newIPInt = newIPInt.Sub(upper, o)
+	case offset > 0:
+		o := big.NewInt(offset)
+		newIPInt = newIPInt.Add(lower, o)
+	}
+	// newIPInt < lower || newIPInt > upper
+	newIP := NewFromBigInt(newIPInt)
+	newIP.SetMaskInplace(ip.mask)
+	if (newIPInt.Cmp(lower) == -1) || (newIPInt.Cmp(upper) == 1) {
+		return &IP{}, fmt.Errorf("result not in subnet range %s+%d !in [%s]", newIP, offset, ip.String())
+	}
+	return newIP, nil
 }
